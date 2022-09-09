6c8daf2656ca545425b7674711a21f6697ee5c3e
Ztellar <b.salamanca@ufromail.cl>
2022-09-09 15:05:18 -0400

    Commit 0

--- Codigo Test Sensores/Codigo Test Sensores.ino
+++ Codigo Test Sensores/Codigo Test Sensores.ino
@@ -13,6 +13,16 @@ void loop() {
   lc = leerSensorLineaCentral();
   ird = leerSensorObstaculoDerecho();
   iri = leerSensorObstaculoIzquierdo();
-  ultra =
-  boton =
+  ultra = leerDistanciaSonar();
+  boton = leerBoton();
+
+  borrarPantalla();
+  escribirPantalla(0, 0, li);
+  escribirPantalla(6, 0, lc);
+  escribirPantalla(13, 0, ld);
+  escribirPantalla(0, 1, iri);
+  escribirPantalla(5, 1, ultra);
+  escribirPantalla(10, 1, boton);
+  escribirPantalla(15, 1, ird);
+  delay(50);
 }
